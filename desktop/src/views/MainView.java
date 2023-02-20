96d0955692dc472ec789897bb32ec3613edccd98
Orosz Levente János <olevente@gmail.oom>
2023-02-20 10:29:58 +0100

    Adatok kész

---
+++ desktop/src/views/MainView.java
@@ -0,0 +1,19 @@
+package views;
+
+import java.util.ArrayList;
+
+public class MainView {
+
+    public MainView()
+    {}
+    
+    public void show(ArrayList<Builders>fatn){
+        Builders[] fatns;
+       
+    {
+
+    }    }
+
+    public void printFantasy(ArrayList<models.Builders> fList) {
+    }
+}
